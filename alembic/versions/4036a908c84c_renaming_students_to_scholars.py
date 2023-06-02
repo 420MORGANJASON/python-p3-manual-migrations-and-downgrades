10dca946f60277084dad6d548007ef85ac320e8b
MORGANJASON <420morganjason@gmail.com>
2023-06-02 05:04:42 +0300

    completed

---
+++ alembic/versions/4036a908c84c_renaming_students_to_scholars.py
@@ -0,0 +1,24 @@
+"""Renaming students to scholars
+
+Revision ID: 4036a908c84c
+Revises: 
+Create Date: 2023-06-02 05:00:52.207136
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4036a908c84c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
